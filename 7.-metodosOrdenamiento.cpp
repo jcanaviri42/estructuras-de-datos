bc19f1af9bc06fb11a2f21b75dd67e25f5c2e26b
josue <jcanaviri20@gmail.com>
2022-02-26 23:19:52 -0400

    order methods

--- 7.-metodosOrdenamiento.cpp
+++ 7.-metodosOrdenamiento.cpp
@@ -67,7 +67,7 @@ void showArray(int array[], int n) {
 void bubbleSort(int array[], int n) {
     // O(n^2)
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n-1; j++) {
+        for (int j = i; j < n-1; j++) {
             if (array[j] > array[j+1]) {
                 swap(array[j], array[j+1]);
             }
