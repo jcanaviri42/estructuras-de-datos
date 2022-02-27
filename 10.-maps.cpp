bb37f69130fe6c4b06745ad9a4a701d65bba8494
josue <jcanaviri20@gmail.com>
2022-02-27 13:52:36 -0400

    map structure

---
+++ 10.-maps.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    map<string, int> m;
+    m["monkey"] = 4;
+    m["banana"] = 3;
+    m["harpsichord"] = 9;
+
+    cout << m["banana"] << "\n";  // 3
+
+    // if the key doesn´t exists map creates a new one
+    cout << m["mango"] << "\n";  // 0
+
+    // The function count checks if the key exists in a map
+    if (m.count("monkey")) {
+        // key exists
+    }
+
+    // The following code prints all the keys and values in a map
+    for (auto x: m) {
+        cout << x.first << " " << x.second << "\n";
+    }
+
+    return 42;
+}
