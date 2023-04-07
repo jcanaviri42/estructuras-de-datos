ea281c35d57728d835123ce0b40001723aa63f33
jcanaviri42 <jcanaviri42@gmail.com>
2023-04-07 09:48:04 -0400

    changes in base.cpp

--- 1.-base.cpp
+++ 1.-base.cpp
@@ -1,28 +1,31 @@
-#include <bits/stdc++.h>  // esta forma nos permite importar muchas librerias a la vez
+#include <bits/stdc++.h> // Esta forma nos permite importar muchas librerias a la vez
 using namespace std;
 
-// Typedef acorta la forma de definir typos
-// por ejemplo ahora no tenemos que escribir:
+// Typedef acorta la forma de definir typos, por ejemplo ahora no tenemos que escribir:
 // - "long long variable" sino mas bien "ll variable"
 typedef long long ll;
-typedef vector<int> vi;  // vector de enteros
-typedef pair<int, int> pi;  // pair de enteros
+typedef vector<int> vi;    // vector de enteros
+typedef pair<int, int> pi; // pair de enteros
 
 // Macros
-#define REP(i,a,b) for (int i = a; i <= b; i++)
-#define SQ(a) (a)*(a) // Los parentesis previenen errores
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define SQ(a) (a) * (a) // Los parentesis previenen errores
 
-int main() {
+int main()
+{
+    // ll - long long
     ll a = 123456789;
     ll b = 987654321;
 
     cout << a * b << "\n";
-    
-    REP(i, 1, 3) {
+
+    // Basicamente mi propio for hecho con macros
+    REP(i, 1, 3)
+    {
         cout << i << "\n";
     }
 
-    cout << "The square root of 9 is: " << SQ(6+3); 
+    cout << "The square root of 9 is: " << SQ(6 + 3);
 
     return 42;
 }
