0b9bd40c1681495a597a3ac80141ad22ba91722d
jcanaviri42 <jcanaviri42@gmail.com>
2023-04-08 23:08:12 -0400

    Structs in c++

--- 2.-structs.cpp
+++ 2.-structs.cpp
@@ -2,32 +2,39 @@
 using namespace std;
 
 // Las estructuras nos permiten definir un propio
-// tipo de dato 
-struct P {
+// tipo de dato
+struct P
+{
     int x, y;
-    bool operator<(const P &p) {
-        if (x != p.x) return x < p.x;
-        else return y < p.y;
+    bool operator<(const P &p)
+    {
+        if (x != p.x)
+            return x < p.x;
+        else
+            return y < p.y;
     }
 };
 
-bool comp (string a, string b) {
-    if (a.size() != b.size()) return a.size() < b.size();
+bool comp(string a, string b)
+{
+    if (a.size() != b.size())
+        return a.size() < b.size();
     return a < b;
 }
 
-int main() {
+int main()
+{
     P point1;
     P point2;
 
     point1.x = 3;
     point1.y = 3;
-    
+
     point2.x = 1;
     point2.y = 1;
 
     bool isSmall = point1 < point2;
-    string message = isSmall ? "Point 1 is small": "Point 2 is small";
+    string message = isSmall ? "Point 1 is small" : "Point 2 is small";
 
     cout << message << endl;
 
