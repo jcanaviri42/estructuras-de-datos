ddd816a5bf76da6ee3c8c5ac9f65a51a2065d0e3
jcanaviri42 <jcanaviri42@gmail.com>
2023-04-09 23:52:54 -0400

    binary search example

--- 6.-binarySearch.cpp
+++ 6.-binarySearch.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int method1(int array[], int n, int target);
 int method2(int array[], int n, int target);
 
-int main() {
+int main()
+{
     int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int size = sizeof(array) / sizeof(array[0]);
     int target = 9;
@@ -15,34 +16,41 @@ int main() {
     return 42;
 }
 
-int method1(int array[], int n, int target) {
+int method1(int array[], int n, int target)
+{
     int a = 0, b = n - 1;
 
     // O(log2 n)
-    while (a <= b) {
+    while (a <= b)
+    {
         int k = (a + b) / 2;
-        if (array[k] == target) {
+        if (array[k] == target)
+        {
             return k;
         }
 
         // Esto reduce a la mitad los elementos a buscar
-        if (array[k] > target) b = k - 1;
-        else a = k + 1;
+        if (array[k] > target)
+            b = k - 1;
+        else
+            a = k + 1;
     }
     return -1;
 }
 
-int method2(int array[], int n, int target) {
+int method2(int array[], int n, int target)
+{
     // O(log2 n)
     int k = 0;
-    
-    for (int b = n/2; b >= 1; b /= 2) {
-        while (k+b < n && array[k+b] <= target) k += b;
+
+    for (int b = n / 2; b >= 1; b /= 2)
+    {
+        while (k + b < n && array[k + b] <= target)
+            k += b;
     }
 
-    if (array[k] == target) {
+    if (array[k] == target)
         return k;
-    } else {
+    else
         return -1;
-    }
 }
